881220eb810cfc158ef2bec812d8a3a012fe4f62
Karim <akskarim17@gmail.com>
2026-05-17 08:08:14 +0600

    preflight checks :)

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import store from "./store.ts";
 import { Provider } from "react-redux";
+// @ts-expect-error maybe later :)
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
